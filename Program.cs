351e0e9deea5737d2cd7efc4a51daf23bd1839e3
Njf092024 <Njf092024@gmail.com>
2025-03-11 11:08:23 +0100

    Register SummonerService in dependency injection container

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SummonerStatsTracker.Services;
+
+var builder = Host.CreateDefaultBuilder(args)
+.configurationServices((context, services)=>
+{
+    services.AddSingleton<SummonerService>();
+})
+.Build();
+
+await.builder.RunAsync();
