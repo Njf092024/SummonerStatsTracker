ea90da1ee9405395bab73bc662e72e8e9ce795a9
Njf092024 <Njf092024@gmail.com>
2025-03-24 10:32:52 +0100

    Add IConfiguration to dependency injection container

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ var builder = Host.CreateDefaultBuilder(args)
 .configurationServices((context, services)=>
 {
     services.AddSingleton<SummonerService>();
+    services.AddSingleton<IConfiguration>(context.configuration);
 })
 .Build();
 
