b744f5a95f3881922d88da4a091d0dd58b8e2334
Njf092024 <Njf092024@gmail.com>
2025-03-25 09:36:14 +0100

    Add Swagger configuration for API documentation

--- Program.cs
+++ Program.cs
@@ -7,3 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwwaggerDoc("v1", new OpenApiInfo { Title = " Summoner API", Version = "v1" });
+});
