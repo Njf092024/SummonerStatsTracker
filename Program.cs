c470a48f8dc78eb5e74848c1e0876c9cf24fd7fd
Njf092024 <Njf092024@gmail.com>
2025-03-25 09:29:57 +0100

    Remove pre-existing code in program

--- Program.cs
+++ Program.cs
@@ -3,12 +3,3 @@ using Microsoft.Extensions.Hosting;
 using SummonerStatsTracker.Services;
 using Microsoft.OpenApi.Models;
 
-var builder = Host.CreateDefaultBuilder(args)
-.ConfigureServices((context, services)=>
-{
-    services.AddSingleton<SummonerService>();
-    services.AddSingleton<IConfiguration>(context.Configuration);
-})
-.Build();
-
-await builder.RunAsync();
